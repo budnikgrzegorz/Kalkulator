c1d2c854116ea9cb39b077a069e1bfbf03b0cef6
Grzegorz_B <budnikgrzegorz@interia.pl>
2017-12-12 20:23:34 +0100

    update
    add event handlers

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,166 +1,69 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Scanner;
-
-public class Controller {
-
-    @FXML
-    AnchorPane AnchorPane;
-    @FXML
-    Button zero;
-    @FXML
-    Button one;
-    @FXML
-    Button two;
-    @FXML
-    Button three;
-    @FXML
-    Button four;
-    @FXML
-    Button five;
-    @FXML
-    Button six;
-    @FXML
-    Button seven;
-    @FXML
-    Button eight;
-    @FXML
-    Button nine;
-    @FXML
-    Button pluse;
-    @FXML
-    Button equal;
-    @FXML
-    Button minus;
-    @FXML
-    Button ratio;
-    @FXML
-    Button quotient;
-    @FXML
-    Button percent;
-    @FXML
-    Button fragment;
+public class Controller implements EventHandler<ActionEvent> {
+    private int firstNumber;
+    private int secondNumber;
+    private String operation;
 
     @FXML
     TextField display;
 
+    public void handleZero(ActionEvent event) {
+        display.setText("0");
+    }
 
-    private ArrayList<Integer> array;
-    private Character sign;
-    private int score = 0;
+    public void handleClear(ActionEvent event) {
+        firstNumber = 0;
+        secondNumber = 0;
+        display.clear();
+    }
 
-    public void initialize() {
-        sohowEqual();
-
-        one.setOnAction(event -> {
-            setParam(1);
-        });
-        two.setOnAction(event -> {
-            setParam(2);
-        });
-        three.setOnAction(event -> {
-            setParam(3);
-        });
-        four.setOnAction(event -> {
-            setParam(4);
-        });
-        five.setOnAction(event -> {
-            setParam(5);
-        });
-        six.setOnAction(event -> {
-            setParam(6);
-        });
-        seven.setOnAction(event -> {
-            setParam(7);
-        });
-        eight.setOnAction(event -> {
-            setParam(8);
-        });
-        nine.setOnAction(event -> {
-            setParam(9);
-        });
-        zero.setOnAction(event -> {
-            setParam(0);
-        });
-        equal.setOnAction(event -> {
-            setSign('=');
-        });
-        minus.setOnAction(event -> {
-            setSign('-');
-        });
-        pluse.setOnAction(event -> {
-            setSign('+');
-        });
-        quotient.setOnAction(event -> {
-            setSign('/');
-        });
-        percent.setOnAction(event -> {
-            setSign('%');
-        });
-        fragment.setOnAction(event -> {
-            setSign(',');
-        });
+    @FXML
+    @Override
+    public void handle(ActionEvent event) {
+        String text = ((Button) event.getSource()).getText();
+        String oldtex = display.getText();
+        String newText = oldtex + text;
+        display.setText(newText);
 
     }
 
-    private void sohowEqual() {
-       display.setText(String.valueOf(score));
-    }
 
-    private void setParam(int param) {
-        if (score != 0) {
-            if (array.get(0).equals(null)) {
-
-                array.add(0, param);
-            } else {
-                array.add(1, param);
-            }
-        } else {
-            array.add(0, getScore());
-            array.add(1, param);
+    @FXML
+    public void handleParamOpperation(ActionEvent event) {
+        this.operation = operation;
+        String oldtex = display.getText();
+        secondNumber = Integer.parseInt(oldtex);
+        switch (operation) {
+            case "+":
+                secondNumber = firstNumber + secondNumber;
+                break;
+            case "-":
+                secondNumber = firstNumber - secondNumber;
+                break;
+            case "*":
+                secondNumber = firstNumber * secondNumber;
+                break;
+            case "/":
+                secondNumber = firstNumber / secondNumber;
+                break;
+            default:
+                break;
         }
+        display.setText(String.valueOf(secondNumber));
     }
 
-    private void showScore() {
-        char sign = getSign();
-        switch (sign) {
-            case '+':
-                display.setText(String.valueOf(array.get(0) + array.get(1)));
-                break;
-            case '-':
-                display.setText(String.valueOf(array.get(0) - array.get(1)));
-                break;
-            case '*':
-                display.setText(String.valueOf(array.get(0) * array.get(1)));
-                break;
-            case '/':
-                display.setText(String.valueOf(array.get(0) * array.get(1)));
-                break;
-
-        }
-
-    }
-
-    public int getScore() {
-        return score;
-    }
-
-    public void setScore(int score) {
-        this.score = score;
-    }
-
-    public Character getSign() {
-        return sign;
-    }
-
-    public void setSign(Character sign) {
-        this.sign = sign;
+    @FXML
+    private void handleExam(ActionEvent event) {
+        String text = display.getText();
+        firstNumber = Integer.parseInt(text);
+        display.setText("");
+        operation = ((Button) event.getSource()).getText();
     }
 }
